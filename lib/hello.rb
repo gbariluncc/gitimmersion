6edde8db22dc16a9cb94cf07907a6cd78716409f
Gary Baril <gbaril@uncc.edu>
2017-03-30 14:02:42 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 # Default is "World"
 # Author: Gary Baril (gbaril@uncc.edu)
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
+puts "Hello, #{my_name}"
